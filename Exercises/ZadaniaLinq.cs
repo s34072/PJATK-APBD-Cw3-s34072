8cddd59ef7ddabcecef4ed2b60b603f6ceb4f742
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-04-01 17:27:52 +0200

    Rozwiązanie zadania 2: Adresy e-mail studentow

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -32,7 +32,7 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie02_AdresyEmailStudentow()
     {
-        throw Niezaimplementowano(nameof(Zadanie02_AdresyEmailStudentow));
+        return DaneUczelni.Studenci.Select(s => s.Email);
     }
 
     /// <summary>
