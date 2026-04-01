26da585c558ce0e39f89044b7249fa9a30f2295d
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-04-01 17:25:50 +0200

    Rozwiązanie zadania 1: Studenci z Warszawy

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -16,7 +16,9 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie01_StudenciZWarszawy()
     {
-        throw Niezaimplementowano(nameof(Zadanie01_StudenciZWarszawy));
+        return DaneUczelni.Studenci
+            .Where(s => s.Miasto == "Warsaw")
+            .Select(s => $"{s.NumerIndeksu} | {s.Imie} {s.Nazwisko} | {s.Miasto}");
     }
 
     /// <summary>
