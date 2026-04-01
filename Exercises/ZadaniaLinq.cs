42abe7b58cecf880b3c1a80ed5c48194b93fb8f5
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-04-01 18:01:03 +0200

    Rozwiązanie zadania 12: Pary student - przedmiot

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -210,7 +210,10 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie12_ParyStudentPrzedmiot()
     {
-        throw Niezaimplementowano(nameof(Zadanie12_ParyStudentPrzedmiot));
+        return from z in DaneUczelni.Zapisy
+            join s in DaneUczelni.Studenci on z.StudentId equals s.Id
+            join p in DaneUczelni.Przedmioty on z.PrzedmiotId equals p.Id
+            select $"{s.Imie} {s.Nazwisko} - {p.Nazwa}";
     }
 
     /// <summary>
