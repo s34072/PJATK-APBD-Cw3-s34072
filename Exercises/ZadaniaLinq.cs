dedc08c8318986ee6fd7bbbd2fbcbaed2ebab597
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-04-01 17:54:59 +0200

    Rozwiązanie zadania 11: Polaczenie studentow i zapisow

--- Exercises/ZadaniaLinq.cs
+++ Exercises/ZadaniaLinq.cs
@@ -192,7 +192,9 @@ public sealed class ZadaniaLinq
     /// </summary>
     public IEnumerable<string> Zadanie11_PolaczStudentowIZapisy()
     {
-        throw Niezaimplementowano(nameof(Zadanie11_PolaczStudentowIZapisy));
+        return from s in DaneUczelni.Studenci
+            join z in DaneUczelni.Zapisy on s.Id equals z.StudentId
+            select $"{s.Imie} {s.Nazwisko} - {z.DataZapisu:yyyy-MM-dd}";
     }
 
     /// <summary>
